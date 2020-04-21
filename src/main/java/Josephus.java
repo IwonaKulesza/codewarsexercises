f83e0a6e693bc3a4995dadcdca413b5902c20e25
Iwona Kulesza <tsubame1990@gmail.com>
2020-04-21 21:04:10 +0200

    still figuring out

--- src/main/java/Josephus.java
+++ src/main/java/Josephus.java
@@ -1,16 +1,32 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class Josephus {
     public static <T> List<T> josephusPermutation(final List<T> items, final int k) {
 
-       Stream.of(items)
-               .skip(2)
-               .limit(2)
-               .findAny()
-               .ifPresent(System.out::println);
+        List temporaryList = new ArrayList<T>();
+        List result = new ArrayList<T>();
+        List list = items;
 
-        return null;
+        while (list.size() >= k) {
+            int listSize = list.size();
+
+            System.out.println("list size " + listSize);
+            for (int i = 0; i < list.size(); i++) {
+                System.out.println(" . "+i);
+                if ((i + 1) % k == 0) {
+                    result.add(list.get(i));
+                    System.out.println("if item "+list.get(i));
+                } else {
+                    temporaryList.add(list.get(i));
+                    System.out.println("else item "+list.get(i));
+                }
+            }
+            list.clear();
+            list.addAll(temporaryList);
+            temporaryList.clear();
+        }
+
+        return result;
     }
 }
--- src/test/java/JosephusTest.java
+++ src/test/java/JosephusTest.java
@@ -8,16 +8,16 @@ import org.junit.Test;
 
 public class JosephusTest {
 
-    @Test
-    public void test1() {
-        josephusTest(new Object[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 1, new Object[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
-    }
+//    @Test
+//    public void test1() {
+//        josephusTest(new Object[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 1, new Object[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+//    }
 
-//        @Test
-//        public void test2() {
-//            josephusTest(new Object[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 2, new Object[]{2, 4, 6, 8, 10, 3, 7, 1, 9, 5});
-//        }
-//
+        @Test
+        public void test2() {
+            josephusTest(new Object[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 2, new Object[]{2, 4, 6, 8, 10, 3, 7, 1, 9, 5});
+        }
+
 //        @Test
 //        public void test3() {
 //            josephusTest(new Object[]{"C", "o", "d", "e", "W", "a", "r", "s"}, 4, new Object[]{"e", "s", "W", "o", "C", "d", "r", "a"});
