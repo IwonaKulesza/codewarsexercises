1e92543e1fbf608d5c257f5770ad62e8d1456bf0
IwonaKulesza <Iwona.Kulesza.1990@gmail.com>
2020-04-27 16:31:55 +0200

    work in progress

--- src/main/java/CarMileage.java
+++ src/main/java/CarMileage.java
@@ -11,15 +11,11 @@ public class CarMileage {
 
         }
 
-        if (number<98){
-            return 0;
-        }
-
         CarMileage cm = new CarMileage();
-        if (cm.interest(number, awesomePhrases)){
+        if ((number>99)&&(cm.interest(number, awesomePhrases))){
             System.out.println("2!");
             return 2;
-        }else if (cm.interest(number+1,awesomePhrases)||cm.interest(number+2,awesomePhrases)){
+        }else if ((number>97)&&(cm.interest(number+1,awesomePhrases)||cm.interest(number+2,awesomePhrases))){
             System.out.println("1!");
             return 1;
         }else return 0;
@@ -34,26 +30,34 @@ public class CarMileage {
             phrases(i, arr);}
 
     private boolean followedByZero(int i){
+        System.out.println("Big num");
         return String.valueOf(i).matches("^\\d0+$");
     }
     private boolean sameNumber(int i){
+        System.out.println("monotone");
         return  String.valueOf(i).matches("^(.)\\1*$");
 
     }
     private boolean increments (int i){
         String string = String.valueOf(i);
-        for (int x = 1; x < string.length()-1; x++){
+
+        for (int x = 1; x < string.length(); x++){
+
             int a = Character.getNumericValue(string.charAt(x-1));
             int b = Character.getNumericValue(string.charAt(x));
-            int c = Character.getNumericValue(string.charAt(x+1));
-            if ((a+1!=b)||(b+1!=c)){
+            if (b == 0){
+                b = 10;
+            }
+            if ((a+1!=b)){
                 return false;
             }
         }
+        System.out.println("inc");
         return true;
     }
     private boolean decrements (int i){
         String string = String.valueOf(i);
+
         for (int x = 1; x < string.length()-1; x++){
             int a = Character.getNumericValue(string.charAt(x-1));
             int b = Character.getNumericValue(string.charAt(x));
@@ -62,6 +66,7 @@ public class CarMileage {
                 return false;
             }
         }
+        System.out.println("dec");
         return true;
     }
     private boolean palindrome (int i) {
@@ -72,6 +77,8 @@ public class CarMileage {
             sum = (sum * 10) + r;
             i = i / 10;
         }
+        System.out.println(i);
+        System.out.println("palindrome"+(temp == sum));
         return (temp == sum);
     }
     private boolean phrases(int i, int arr[]){
@@ -81,3 +88,18 @@ public class CarMileage {
 
 
 }
+//    public static boolean isReallyInteresting(int number, final int[] awesomePhrases) {
+//        return Stream.<Predicate<String>>of(
+//                s -> s.matches("\\d0+"),
+//                s -> new StringBuilder(s).reverse().toString().equals(s),
+//                s -> "1234567890".contains(s),
+//                s -> "9876543210".contains(s),
+//                s -> Arrays.stream(awesomePhrases).anyMatch(n -> parseInt(s) == n)
+//        ).anyMatch( p -> number > 99 && p.test(Integer.toString(number)));
+//    }
+//
+//    public static int isInteresting(int number, int[] awesomePhrases) {
+//        return isReallyInteresting(number, awesomePhrases) ? 2 :
+//                isReallyInteresting(number + 1, awesomePhrases) ? 1 :
+//                        isReallyInteresting(number + 2, awesomePhrases) ? 1 : 0;
+//    }
---
+++ src/main/java/Dinglemouse.java
@@ -0,0 +1,11 @@
+class Dinglemouse {
+
+    private static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ?!@#&()|<>.:=-+*/0123456789";
+
+    public static String[] flapDisplay(final String[] lines, final int[][] rotors) {
+
+
+               return new String[0];
+    }
+
+}
